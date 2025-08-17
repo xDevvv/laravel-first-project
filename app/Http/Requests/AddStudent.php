fb4cfef8012d3556468a1546fdaef3d5679c68ee
xDevvv <zabsfreks@gmail.com>
2025-08-17 19:02:07 +0800

    Changing authorize() from false to true

--- app/Http/Requests/AddStudent.php
+++ app/Http/Requests/AddStudent.php
@@ -11,7 +11,7 @@ class AddStudent extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/StoreAcceptRequest.php
+++ app/Http/Requests/StoreAcceptRequest.php
@@ -12,7 +12,7 @@ class StoreAcceptRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
