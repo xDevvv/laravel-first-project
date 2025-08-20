5fd90d42020d165535aa73eb319b4cbe6b9b2b8f
xDevvv <zabsfreks@gmail.com>
2025-08-20 22:53:24 +0800

    Register Policy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,9 +3,13 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Models\Students;
+use App\Policies\StudentPolicy;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
+
     /**
      * Register any application services.
      */
@@ -19,6 +23,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::policy(Students::class, StudentPolicy::class);
     }
 }
