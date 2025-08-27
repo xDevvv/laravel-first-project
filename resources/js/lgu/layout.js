d419b7d5220c14ac5acfcd520e00563b514a85a5
xDevvv <zabsfreks@gmail.com>
2025-08-27 21:24:05 +0800

    Add Additional Layout

--- resources/js/lgu/layout.js
+++ resources/js/lgu/layout.js
@@ -1,4 +1,5 @@
 export function slacksSkirt_poloBlouse_table_header(productHeader) {
+    console.log(productHeader.product1);
     return `
         <tr>
             <th style="text-align: center; border-inline: 1px solid var(--light-blue); border-block: 1px solid var(--light-blue); font-size: 12px;" scope="col">Size</th>
@@ -114,7 +115,6 @@ export function school_supplies_table_header() {
 }
 
 export function school_supplies_table_body(studentData) {
-    console.log(studentData);
     return `
         <tr>
             <td style="text-align: center; border-inline: 1px solid var(--light-blue); border-block: 1px solid var(--light-blue); font-size: 12px;">Male</td>
@@ -129,3 +129,138 @@ export function school_supplies_table_body(studentData) {
     `;
 }
 
+export function dataInsertionLayout(product, {data}) {
+    if(product == 'slacks_skirt_size' || product == 'polo_blouse_size') {
+        console.log(data.total);
+        document.querySelector('.overall-layout-table').innerHTML = `
+            <thead>
+                ${slacksSkirt_poloBlouse_table_header({product1: data.productHeader.product1, product2: data.productHeader.product2})}
+            </thead>
+            <tbody class="student-table-body overall-layout-table-body">
+                ${slacksSkirt_poloBlouse_table_body(data.sizeTotals)}
+            </tbody>
+        `;
+
+        document.querySelector('.overall-table-total').innerHTML = `
+            <thead>
+                <tr>
+                    <th style="text-align: center; border-inline: 1px solid var(--light-blue); border-block: 1px solid var(--light-blue); font-size: 12px;" scope="col">Total</th>
+                    <th style="text-align: center; border-inline: 1px solid var(--light-blue); border-block: 1px solid var(--light-blue); font-size: 12px;" scope="col">${data.productHeader.product1}</th>
+                    <th style="text-align: center; border-inline: 1px solid var(--light-blue); border-block: 1px solid var(--light-blue); font-size: 12px;" scope="col">${data.productHeader.product2}</th>
+                </tr>
+            </thead>
+            <tbody class="student-table-body">
+                <tr>
+                    <td style="text-align: center; border-inline: 1px solid var(--light-blue); border-block: 1px solid var(--light-blue); font-size: 12px;">${data.studentTotal.boysOverallTotal + data.studentTotal.girlsOverallTotal}</td>
+                    <td style="text-align: center; border-inline: 1px solid var(--light-blue); border-block: 1px solid var(--light-blue); font-size: 12px;">${data.studentTotal.boysOverallTotal}</td>
+                    <td style="text-align: center; border-inline: 1px solid var(--light-blue); border-block: 1px solid var(--light-blue); font-size: 12px;">${data.studentTotal.girlsOverallTotal}</td>
+                </tr>
+            </tbody>
+        `;
+    }
+
+    if(product == 't_shirt_size' || product == 'pants_size') {
+        console.log(data);
+        document.querySelector('.overall-layout-table').innerHTML = `
+            <thead>
+                ${shirt_and_pants_table_header(data.productHeader)}
+            </thead>
+            <tbody class="student-table-body overall-layout-table-body">
+                ${shirt_and_pants_table_body(data.sizeTotals)}
+            </tbody>
+        `;
+
+        document.querySelector('.overall-table-total').innerHTML = `
+            <thead>
+                ${shirt_and_pants_table_header(data.productHeader)}
+            </thead>
+            <tbody class="student-table-body">
+                <tr>
+                    <td style="text-align: center; border-inline: 1px solid var(--light-blue); border-block: 1px solid var(--light-blue); font-size: 12px;">Total</td>
+                    <td style="text-align: center; border-inline: 1px solid var(--light-blue); border-block: 1px solid var(--light-blue); font-size: 12px;">${data.studentTotal.boysOverallTotal + data.studentTotal.girlsOverallTotal}</td>
+                </tr>
+            </tbody>
+        `;
+    }
+
+    if(product == 'shoe_size') {
+        document.querySelector('.overall-layout-table').innerHTML = `
+            <thead>
+                ${shoe_size_table_header()}
+            </thead>
+            <tbody class="student-table-body overall-layout-table-body">
+                ${shoe_size_table_body(data.shoeSize)}
+            </tbody>
+        `;
+        document.querySelector('.overall-table-total').innerHTML = `
+            <thead>
+                ${shoe_size_table_header()}
+            </thead>
+            <tbody class="student-table-body">
+                <tr>
+                    <td style="text-align: center; border-inline: 1px solid var(--light-blue); border-block: 1px solid var(--light-blue); font-size: 12px;">${data.totalSize}</td>
+                    <td style="text-align: center; border-inline: 1px solid var(--light-blue); border-block: 1px solid var(--light-blue); font-size: 12px;">${data.totalShoes} PCS</td>
+                </tr>
+            </tbody>
+        `;
+    }
+
+    if(product == 'school_supplies') {
+        console.log(data.schoolSupplies);
+        document.querySelector('.overall-layout-table').innerHTML = `
+            <thead>
+                ${school_supplies_table_header()}
+            </thead>
+            <tbody class="student-table-body overall-layout-table-body">
+                ${school_supplies_table_body(data)}
+            </tbody>
+        `;
+        document.querySelector('.overall-table-total').innerHTML = `
+            <thead>
+                <tr>
+                    <th style="text-align: center; border-inline: 1px solid var(--light-blue); border-block: 1px solid var(--light-blue); font-size: 12px;" scope="col">Gender</th>
+                    <th style="text-align: center; border-inline: 1px solid var(--light-blue); border-block: 1px solid var(--light-blue); font-size: 12px;" scope="col">No</th>
+                    <th style="text-align: center; border-inline: 1px solid var(--light-blue); border-block: 1px solid var(--light-blue); font-size: 12px;" scope="col">Yes</th>
+                </tr> 
+            </thead>
+            <tbody class="student-table-body">
+                <tr>
+                    <td style="text-align: center; border-inline: 1px solid var(--light-blue); border-block: 1px solid var(--light-blue); font-size: 12px;">Total</td>
+                    <td style="text-align: center; border-inline: 1px solid var(--light-blue); border-block: 1px solid var(--light-blue); font-size: 12px;">${data.male.no + data.female.no}</td>
+                    <td style="text-align: center; border-inline: 1px solid var(--light-blue); border-block: 1px solid var(--light-blue); font-size: 12px;">${data.male.yes + data.female.yes} PCS</td>
+                </tr>
+            </tbody>
+        `;
+    }
+}
+
+export function fetchStudentData(data) {
+    let hasHeader = false;
+    let studentCount = 0;
+    for(let i = 0; i < data.length; i++) {
+        
+        const tableBody = document.querySelector(`.${data[i].section}-student-table-body`);
+        console.log(tableBody);
+        if(!hasHeader) {
+            let dataHeader = document.createElement('tr');
+            dataHeader.innerHTML = `
+                <td style="text-align: center; font-size: 12px;"></td>
+                <td style="text-align: center; font-size: 12px; color: red;">Boys</td>
+                <td style="text-align: center; font-size: 12px;"></td>
+            `;
+
+            tableBody.appendChild(dataHeader);
+            hasHeader = true;
+        }
+
+        studentCount++;
+        const row = document.createElement('tr');
+        row.innerHTML = `
+            <td style="text-align: center; font-size: 12px;">${studentCount}</td>
+            <td style="text-align: center; font-size: 12px;">${data[i].first_name} ${data[i].last_name}</td>
+            <td style="text-align: center; font-size: 12px;">${data[i].slacks_skirt_size}</td>
+        `;
+        
+        tableBody.appendChild(row);
+    }
+}
